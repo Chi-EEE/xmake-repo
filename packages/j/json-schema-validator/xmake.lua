7ffce754a5fce94879ad63a5ce8f9adcaa54f988
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-09-29 16:22:34 +0100

    json-schema-validator: Use XMake instead of CMake

--- packages/j/json-schema-validator/xmake.lua
+++ packages/j/json-schema-validator/xmake.lua
@@ -6,32 +6,28 @@ package("json-schema-validator")
              "https://github.com/pboettch/json-schema-validator.git")
     add_versions("2.1.0", "83f61d8112f485e0d3f1e72d51610ba3924b179926a8376aef3c038770faf202")
 
-    add_deps("cmake", "nlohmann_json")
-
-    if is_plat("mingw", "windows") then
-        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
-    end
-
-    on_install("linux", "bsd", "windows", "macosx", "iphoneos", "mingw", "cross", function (package)
+    add_deps("nlohmann_json")
+    
+    on_install(function (package)
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
         local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DJSON_VALIDATOR_BUILD_EXAMPLES=OFF")
-        table.insert(configs, "-DJSON_VALIDATOR_BUILD_TESTS=OFF")
-        import("package.tools.cmake").install(package, configs)
+        if package:config("shared") then
+            package:add("defines", "JSON_SCHEMA_VALIDATOR_EXPORTS")
+        end
+        import("package.tools.xmake").install(package, configs)
     end)
 
-    on_test(function (package)
-        assert(package:check_cxxsnippets({test = [[
-        #include <nlohmann/json-schema.hpp>
+    -- on_test(function (package)
+    --     assert(package:check_cxxsnippets({test = [[
+    --     #include <nlohmann/json-schema.hpp>
 
-        using nlohmann::json;
-        using nlohmann::json_schema::json_validator;
+    --     using nlohmann::json;
+    --     using nlohmann::json_schema::json_validator;
 
-        static json person_schema = R"({})"_json;
-        static void test() {
-            json_validator validator;
-            validator.set_root_schema(person_schema);
-        }
-        ]]}, {configs = {languages = "c++17"}}))
-    end)
+    --     static json person_schema = R"({})"_json;
+    --     static void test() {
+    --         json_validator validator;
+    --         validator.set_root_schema(person_schema);
+    --     }
+    --     ]]}, {configs = {languages = "c++17"}}))
+    -- end)
