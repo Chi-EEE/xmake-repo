0a937f81e677431c163899280d591daae37523a6
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-11-18 20:49:23 +0000

    Update xmake.lua

--- packages/q/quickjs-ng/xmake.lua
+++ packages/q/quickjs-ng/xmake.lua
@@ -16,24 +16,11 @@ package("quickjs-ng")
         add_syslinks("m", "pthread")
     end
 
+    add_deps("mimalloc")
     add_deps("cmake")
 
-    if on_check then
-        on_check("windows", function (package)
-            local vs_toolset = package:toolchain("msvc"):config("vs_toolset")
-            if vs_toolset then
-                local vs_toolset_ver = import("core.base.semver").new(vs_toolset)
-                local minor = vs_toolset_ver:minor()
-                assert(minor and minor >= 30, "package(quickjs-ng) require vs_toolset >= 14.3")
-            end
-        end)
-    end
-
     on_install(function (package)
-        io.replace("CMakeLists.txt", "xcheck_add_c_compiler_flag(-Werror)", "", {plain = true})
-        io.replace("CMakeLists.txt", "if(NOT WIN32 AND NOT EMSCRIPTEN)", "if(0)", {plain = true})
-
-        local configs = {}
+        local configs = {"-DBUILD_CLI_WITH_STATIC_MIMALLOC=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DCONFIG_ASAN=" .. (package:config("asan") and "ON" or "OFF"))
