eb7c7314c256f84166119dc7aa9f0b8ffb538cdf
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-11-18 20:54:49 +0000

    quickjs-ng: update version

--- packages/q/quickjs-ng/xmake.lua
+++ packages/q/quickjs-ng/xmake.lua
@@ -3,11 +3,9 @@ package("quickjs-ng")
     set_description("QuickJS, the Next Generation: a mighty JavaScript engine")
     set_license("MIT")
 
-    add_urls("https://github.com/quickjs-ng/quickjs/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/quickjs-ng/quickjs.git", {submodules = false})
+    add_urls("https://github.com/quickjs-ng/quickjs.git", {submodules = false})
 
-    add_versions("v0.6.1", "276edbb30896cdf2eee12a8bdb5b9c1cc2734eac8c898de6d52268ae201e614d")
-    add_versions("v0.5.0", "41212a6fb84bfe07d61772c02513734b7a06465843ba8f76f1ce1e5df866f489")
+    add_versions("18.11.2024", "55b67a6591071b2ada8895a7000e0fec5f348016")
 
     add_configs("libc", {description = "Build standard library modules as part of the library", default = false, type = "boolean"})
 
@@ -15,24 +13,11 @@ package("quickjs-ng")
         add_syslinks("m", "pthread")
     end
 
+    add_deps("mimalloc")
     add_deps("cmake")
 
-    if on_check then
-        on_check("windows", function (package)
-            local vs_toolset = package:toolchain("msvc"):config("vs_toolset")
-            if vs_toolset then
-                local vs_toolset_ver = import("core.base.semver").new(vs_toolset)
-                local minor = vs_toolset_ver:minor()
-                assert(minor and minor >= 30, "package(quickjs-ng) require vs_toolset >= 14.3")
-            end
-        end)
-    end
-
-    on_install("!iphoneos and (!windows or windows|!x86)", function (package)
-        io.replace("CMakeLists.txt", "xcheck_add_c_compiler_flag(-Werror)", "", {plain = true})
-        io.replace("CMakeLists.txt", "if(NOT WIN32 AND NOT EMSCRIPTEN)", "if(0)", {plain = true})
-
-        local configs = {}
+    on_install(function (package)
+        local configs = {"-DBUILD_CLI_WITH_STATIC_MIMALLOC=ON"}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DCONFIG_ASAN=" .. (package:config("asan") and "ON" or "OFF"))
