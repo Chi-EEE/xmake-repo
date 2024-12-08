7c37e998c0db2196252de5f1d085c8b4055c1585
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-12-08 11:51:45 +0000

    Update xmake.lua

--- packages/p/package_with_broken_on_load/xmake.lua
+++ packages/p/package_with_broken_on_load/xmake.lua
@@ -1,9 +1,18 @@
 package("package_with_broken_on_load")
     add_configs("hide_existing_package_error", {default = is_plat("windows"), type = "boolean"})
 
-    on_load("windows", function (package)
+    if on_check then
+        on_check(function (package)
+            if package:is_plat("linux") then
+                raise("This package does not support Linux for ... reasons.")
+            end
+        end)
+    end
+
+    on_load(function (package)
         if package:config("hide_existing_package_error") then
             add_syslinks("advapi32")
+            raise("This package is broken on purpose.")
         end
     end)
 
